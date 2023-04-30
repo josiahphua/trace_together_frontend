e9b29d7515d5a87f8daf89b3811152211a435761
josiah <josiahphuajy@gmail.com>
2023-04-30 14:18:12 +0800

    added todo component, requires refinement

--- src/component/todo/Todo.tsx
+++ src/component/todo/Todo.tsx
@@ -5,32 +5,56 @@ import axios from 'axios';
 type Props = {}
 
 interface ITodo {
+createdAt: Date
+createdBy: String
+description: String
+email: String
+id: Number
+priority: String
+status: String
+title: String
+todoEndDate: String
+todoStartDate: String
 
 }
 
 const Todo: React.FC = (props: Props) => {
-    const [allTodos, setAllTodos] = useState<any>("")
+    const [allTodos, setAllTodos] = useState<ITodo[]>([])
+    const todoItemMap:
+    {
+      [id: number]:
+      {
+        id: number,
+        title: String,
+        description: string,
+        createdBy: string
+      }
+    } = {};
 
     useEffect(() => {
-        // const getTodo = async () => {
-        //     try {
-        //         const data = await axios.get(`http://localhost:8080/api/todos`, {})
-        //         console.log(data)
-        //         setAllTodos(data)
-        //         console.log(allTodos)
-        //     } catch (error) {
+        const getTodo = async () => {
+            try {
+                const {data} = await axios.get(`http://localhost:8080/api/todos`, {})
+                setAllTodos(data)
+            } catch (error) {
                 
-        //     }
-        // }
-        // getTodo()
-    })
+            }
+        }
+        getTodo()
+    },[])
 
+    console.log("all todos set", allTodos)
 
   return (
     <div>
-        <Card>
-            This is the card for Todo?
+        {allTodos.map((todoItem) => (
+        <Card key={todoItem.id.toString()}>
+          <h2>{todoItem.title}</h2>
+          <p>Todo item ID: {todoItem.id.toString()}</p>
+          <p>Description: {todoItem.description}</p>
+          <p>Created by: {todoItem.createdBy}</p>
         </Card>
+      ))}
     </div>
   )
 }
